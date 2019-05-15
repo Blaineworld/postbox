0200cff2fa4173b8224db804270daef4ac7a4e92
Blaineworld <40525428+Blaineworld@users.noreply.github.com>
2019-05-15 15:04:11 -0500

    Alpha 0.2 update!
    + Added this changelog! :)
    + Hovering the mouse over a post's title now shows its ID.
    + Introduced an upvote / downvote system, currently known as "points."
       Posts may be upvoted or downvoted every five minutes. Once a post is
       voted, five minutes must pass before another post can be voted. The
       cooldown might be changed in the future to prevent addictions to
       self-upvoting, but it's basically like Reddit but with cooldowns.
       Making it actually took much longer than I thought it would.
    + Introduced new styles for <code> elements, which are now also copied
       automatically just by triple-clicking them, because we all copy half
       our code from GitHub. The other 90% is from StackOverflow. When you
       hover over it, it fades in to a very l33t color scheme.
    + List items now glow white when hovered over and are now copied with
       a triple-click.
    + There's a manual now! Access it with the button in the top right.
       It's not even close to being ready though.
    + Tried to add the auto-deletion thing the manual mentions but it was
       saying some date in 1970 for absolutely no reason and it makes me so
       freaking mad!!!!!!!!!! That's why I disabled it and you won't see it
       in this release. Hopefully I can fix whatever this heckin' bug is.
    * Improved the block quote styles.
    * Posts are now sorted by their newness AND their score (since scores just got added).
    * Tweaked what cursors are used where.

--- Postbox Web Interface/script.js
+++ Postbox Web Interface/script.js
@@ -2,18 +2,22 @@
 inputtitle.placeholder = postbox.translate("gui.post.titlePlaceholder", inputtitle.maxLength = postbox.getTitleCharacterLimit());
 inputcontent.placeholder = postbox.translate("gui.post.contentPlaceholder", inputcontent.maxLength = postbox.getCharacterLimit());
 
-function onLinkClicked(a) {
-	// Processes the link and opens it in a little window thingy.
-	window.open(a.src);
-}
+var noVoteStyles = document.querySelector("#no-vote-styles");
+setTimeout(disableNoVoteStyles, postbox.voteCooldown());
 
 function render(post) {
 	// Render a post.
-	var e;
+	var e, f;
 	(e = posts.insertBefore(document.createElement("FIELDSET"), posts.children[0])).className = "post";
 	e.innerHTML = post.html;
-	(e = e.appendChild(document.createElement("LEGEND"))).innerHTML = "<span class=\"date\">" + post.posted + "</span> " + ((post.title||'').replace(/</g, "&lt;").replace(/>/g, "&gt;")||'<i>Untitled</i>');
+	(f = e.appendChild(document.createElement("P"))).setAttribute("id", "post-id");
+	f.innerText = post.identifier;
+	(e = e.appendChild(document.createElement("LEGEND"))).innerHTML = "<span class=\"date\">" + post.posted + "</span> " + ((post.title || "").replace(/</g, "&lt;").replace(/>/g, "&gt;") || "<i>Untitled</i>") + "<span title=\"" + post.points + " points\" class=\"vote\"><span class=\"vote-up\">▲</span><span id=\"points\">" + post.points + "</span><span class=\"vote-down\">▼</span></span>";
+	e.title = "ID: " + post.identifier;
 }
+
+postbox.removeExpired();
+setInterval(postbox.removeExpired, 120000); // Remove old posts every ~2 minutes.
 
 var p = [];
 
@@ -23,9 +27,34 @@ for (var i = 0; i < ids.length; i++) {
 }
 
 p.sort(function(a, b) {
-	return a.date - b.date;
+	return (a.date - b.date) + ((a.points - b.points) * 100000);
 });
 
 for (var i = 0; i < p.length; i++) {
 	render(p[i]);
 }
+
+function disableNoVoteStyles() {
+	noVoteStyles.disabled = true;
+}
+
+addEventListener("click", function(event) {
+	if (event.detail === 3) {
+		if (event.target.tagName.toUpperCase() === "LI" || event.target.tagName.toUpperCase() === "CODE") {
+			document.execCommand("Copy");
+			console.info("Copied!");
+		}
+	} else {
+		if (event.target.className.replace("down", "up") === "vote-up" && !postbox.voteCooldown()) {
+			if (event.target.className === "vote-down") {
+				postbox.downvote(event.target.parentElement.parentElement.parentElement.querySelector("#post-id").innerText);
+				event.target.parentElement.querySelector("#points").innerText--;
+			} else {
+				postbox.upvote(event.target.parentElement.parentElement.parentElement.querySelector("#post-id").innerText);
+				event.target.parentElement.querySelector("#points").innerText++;
+			}
+			noVoteStyles.disabled = false;
+			setTimeout(disableNoVoteStyles, postbox.voteCooldown());
+		}
+	}
+});
